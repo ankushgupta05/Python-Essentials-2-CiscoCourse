c5282e326e4ad69d4d306e3e58f0280eb87019ef
Ankush Gupta <144006768+ankushgupta05@users.noreply.github.com>
2025-05-04 14:52:52 +0530

    Update LabWorkPythonCoding.py

--- moudule_2/LabWorkPythonCoding.py
+++ moudule_2/LabWorkPythonCoding.py
@@ -296,3 +296,53 @@ sudoku = [list(map(int, input().strip())) for _ in range(9)]
 | **Fails if input is**  | `295743861` (will give just 1 number, not 9)                | `2 9 5 7 4 3 8 6 1` (will raise ValueError)             |
 | **Common use**         | Used when user gives digits **with spaces**                 | Used when user gives digits **without spaces**          |
 
+
+
+
+
+Q 6)
+2.5.11   LAB   Sudoku
+As you probably know, Sudoku is a number-placing puzzle played on a 9x9 board. The player has to fill the board in a very specific way:
+
+each row of the board must contain all digits from 0 to 9 (the order doesn't matter)
+each column of the board must contain all digits from 0 to 9 (again, the order doesn't matter)
+each of the nine 3x3 "tiles" (we will name them "sub-squares") of the table must contain all digits from 0 to 9.
+If you need more details, you can find them here.
+
+Your task is to write a program which:
+reads 9 rows of the Sudoku, each containing 9 digits (check carefully if the data entered are valid)
+outputs Yes if the Sudoku is valid, and No otherwise.
+Test your code using the data we've provided.
+
+Test Data
+#1
+Sample input:
+295743861
+431865927
+876192543
+387459216
+612387495
+549216738
+763524189
+928671354
+154938672
+
+Sample output:
+Yes
+
+#2
+Sample input:
+195743862
+431865927
+876192543
+387459216
+612387495
+549216738
+763524189
+928671354
+254938671
+
+Sample output:
+No
+
+
