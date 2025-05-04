6cdcaaf3ced2c79172c5d855dbd607363a680e10
Ankush Gupta <144006768+ankushgupta05@users.noreply.github.com>
2025-05-04 12:28:25 +0530

    Update LabWorkPythonCoding.py

--- moudule_2/LabWorkPythonCoding.py
+++ moudule_2/LabWorkPythonCoding.py
@@ -137,3 +137,64 @@ else:
 
 
 
+
+
+4)
+2.5.9   LAB   The Digit of Life
+Some say that the Digit of Life is a digit evaluated using somebody's birthday. It's simple – you just need to sum all the digits of the date. If the result contains more than one digit, you have to repeat the addition until you get exactly one digit. For example:
+
+1 January 2017 = 2017 01 01
+2 + 0 + 1 + 7 + 0 + 1 + 0 + 1 = 12
+1 + 2 = 3
+3 is the digit we searched for and found.
+
+Your task is to write a program which:
+
+asks the user her/his birthday (in the format YYYYMMDD, or YYYYDDMM, or MMDDYYYY – actually, the order of the digits doesn't matter)
+outputs the Digit of Life for the date.
+Test your code using the data we've provided.
+
+Test Data
+#1
+Sample input:
+19991229
+
+Sample output:
+6
+
+#2
+Sample input:
+20000101
+
+Sample output:
+4
+
+
+// code by me:
+
+x = int(input())
+
+temp = 0
+while(1):
+    temp += x%10
+    x //= 10
+    if x == 0:
+        break;
+
+x = temp
+temp = 0
+while(1):
+    temp += x%10
+    x //= 10
+    if x == 0:
+        break;
+print(temp)
+
+// code by chatgpt
+def sum_digits(n):
+    return sum(int(d) for d in str(n))
+
+x = int(input())
+print(sum_digits(sum_digits(x)))
+
+
